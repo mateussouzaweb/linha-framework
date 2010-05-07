7aff33f35b9de822d3e76ce5910d6bb84187abf0
Mateus Souza <mateussouzaweb@gmail.com>
2010-05-07 14:46:34 -0300

    alteração de monitoramento e adição de opções Live + Callback

--- xhtml/desenvolvimento/js/jquery.linha.acord.js
+++ xhtml/desenvolvimento/js/jquery.linha.acord.js
@@ -10,7 +10,7 @@
 * @copyright		(c) 2010 Mateus Souza
 * @license			MIT and GPL License - http://www.opensource.org/licenses/mit-license.php || http://www.gnu.org/licenses/gpl.html
 * 
-* @ultima-revisao   06/04/10 as 14:26 | nº 9
+* @ultima-revisao   07/04/10 as 14:35 | nº 10
 */
 (function($){
 	$.fn.acord = function(options){
@@ -39,7 +39,10 @@
 				easingIn: 'swing',									//Animação com easyng na entrada (IN)...
 				easingOut: 'swing',									//Animação com easyng na saída (OUT)...
 				
-				onAcord: null 										//Callback
+				onAcord: null, 										//Callback
+				
+				live: true,											//Abilitar o monitoramento live
+				liveTempo: 100										//Tempo entra cada checagem, em milisegundos
 		};	
 		
 		var o = $.extend(padrao, options),
@@ -48,63 +51,73 @@
 			af = o.classeFilhoAtual;
 		
 		if(elem === undefined){elem = $(o.seletor);}
+		elem.elements = [];
 		
 		/**
 		 * Delegando eventos....o resultado seria o mesmo que each
 		 * Usado para registro em accordions futuros
 		 */
 		$d.delegate(elem.selector, 'iniciaAcord', function(){
-			var $this = $(this);
+			
+			var elems = elem.elements,
+			els  =  $(elem.selector, elem.context),
+			nEls = els.not(elems);
+			elem.elements = els;
 
 			/**
-			 * Fix para erros de animação
+			 * Retorna o processamento dos elementos q passaram
 			 */
-			$(o.filho, this).each(function(){$(this).css('height', $(this).height() + 'px').hide();});
-			if (o.sempreUm) {
-				$(o.filho, this).eq(o.inicial - 1).addClass(af).show().prev(o.pai).addClass(ap);
-			}
-			
-			/**
-			 * Altura automática
-			 */
-			if(o.autoHeight){
-				var h = 0; 
-				$(o.filho, this).each(function(){
-					h = Math.max(h, $(this).outerHeight());
-				}).height(h);
-				
-				$this.height($this.height()).css({overflow: 'hidden'});
-			}
-			
-			/**
-			 * Bind no evento e setagem de valores
-			 */
-			$(o.pai, this).bind(o.evento, function(){
-				if ($(this).hasClass(o.classeAjax)) {
-					return ajaxAcord($this, $(this), $(this).next());
+			nEls.each(function() {
+				var $t = $(this);
+				/**
+				 * Fix para erros de animação
+				 */
+				$(o.filho, this).each(function(){$(this).css('height', $(this).height() + 'px').hide();});
+				if (o.sempreUm) {
+					$(o.filho, this).eq(o.inicial - 1).addClass(af).show().prev(o.pai).addClass(ap);
 				}
-				return animaAcord($this, $(this), $(this).next());
+				
+				/**
+				 * Altura automática
+				 */
+				if(o.autoHeight){
+					var h = 0; 
+					$(o.filho, this).each(function(){
+						h = Math.max(h, $(this).outerHeight());
+					}).height(h);
+					
+					$t.height($t.height()).css({overflow: 'hidden'});
+				}
+				
+				/**
+				 * Bind no evento e setagem de valores
+				 */
+				$(o.pai, this).bind(o.evento, function(){
+					if ($(this).hasClass(o.classeAjax)) {
+						return ajaxAcord($t, $(this), $(this).next());
+					}
+					return animaAcord($t, $(this), $(this).next());
+				});
+				
+				/**
+				 * Checa se o inicial é em ajax e já manda a requisição
+				 */
+				if(o.sempreUm && $('.'+ap, this).hasClass(o.classeAjax)){
+					$('.'+ap, this).trigger(o.evento);
+				}
 			});
-			
-			/**
-			 * Checa se o inicial é em ajax e já manda a requisição
-			 */
-			if(o.sempreUm && $('.'+ap, this).hasClass(o.classeAjax)){
-				$('.'+ap, this).trigger(o.evento);
-			}
 			
 		});
 		
 		/**
-		 * Trigger inicial e monitoramento ajax
+		 * Trigger inicial e monitoramento DOM
 		 */
-		if(elem.length){elem.trigger('iniciaAcord');}
-		else {
-			$d.bind('ajaxComplete', function(o, xhr, url){
-				if (xhr.readyState == 4 && xhr.status == 200 && $(elem.selector).length) {
-					$(elem.selector).trigger('iniciaAcord');
-				}
-			});
+		if(o.live){
+			setInterval(function(){
+				$(elem.selector).trigger('iniciaAcord');
+			}, o.liveTempo);
+		}else{
+			if(elem.length){elem.trigger('iniciaAcord');}
 		}
 		
 		/**
