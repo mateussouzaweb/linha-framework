2e34e6c314190242315465799648f6754df98414
Mateus Souza <mateussouzaweb@gmail.com>
2010-05-28 10:07:34 -0300

    Finalização Linha Valida 1.1 + demos

--- desenvolvimento/js/jquery.linha.valida.js
+++ desenvolvimento/js/jquery.linha.valida.js
@@ -3,7 +3,7 @@
 * @version			1.1
 * @descripton		Plugin Jquery para validação de formularios
 * 					Plugin extensével e customizável
-*					MODO DE USAR $.form({opcoes}); || $('.classeTal').form({opcoes});
+*					MODO DE USAR $.valida({opcoes}); || $('.classeTal').valida({opcoes});
 *
 * @author			Mateus Souza
 * @author-email		mateussouzaweb@gmail.com
@@ -11,7 +11,7 @@
 * @copyright		(c) 2010 Mateus Souza
 * @license			MIT and GPL License - http://www.opensource.org/licenses/mit-license.php || http://www.gnu.org/licenses/gpl.html
 * 
-* @ultima-revisao   24/05/10 as 15:47 | nº 4
+* @ultima-revisao   26/05/10 as 18:31 | nº 7
 */
 (function($){
 	
@@ -31,6 +31,8 @@
 			
 			classeRegex: 'regex',					//Classe para validação personalizada
 			atributoRegex: 'regex',					//Atributo para regex | validação personalizada
+			
+			ValorSelectPadrao: 0,					//Valor padrão para validação do campo select
 				
 			redireciona: true,						//Caso true o formulário é enviado via ajax, false redireciona para outra página
 			
@@ -63,7 +65,7 @@
 			 */
 			nEls.each(function(){
 				
-				var $t = $(this);
+				var $form = $(this);
 			
 				/**
 				 * Checa se é um formulário
@@ -78,33 +80,27 @@
 					/**
 					 * Começa a validação
 					 */
-					var resultado = $.valida.form(o, elem);
-					//Erro por aqui
-					/**
-					 * Se passar
-					 */
-					if(!resultado){
-						if(o.redireciona){
+					var resultado = $.valida.form(o, nEls);
+					
+					if(resultado){
+						if(!o.redireciona){
 							/**
 							 * Callback
 							 */
 							if ($.isFunction(o.onPassa)) {
-								o.onPassa.apply(this, new Array($t, o));
+								o.onPassa.apply($form, new Array($form, o));
 							}
 							return false;
 						}else{
 							return resultado;
 						}
-					
-					/**
-					 * Se não
-					 */	
+
 					}else{
 						/**
 						 * Callback
 						 */
 						if ($.isFunction(o.onNaoPassa)) {
-							o.onNaoPassa.apply(this, new Array($t, o));
+							o.onNaoPassa.apply($form, new Array($form, o));
 						}
 						return false;
 					}			
@@ -139,7 +135,7 @@
 		 * Faz o processo em cada entrada
 		 */
 		elem.each(function(){
-			
+
 			$(this).find('input, textarea, select, checkbox, radio').not(':submit').each(function(){
 				
 				var $t = $(this);
@@ -156,7 +152,14 @@
 						o.onValida.apply(this, new Array($t, o));
 					}
 					
-					var tValido = validaInput(t,o);
+					/**
+					 * Separa pelo tipo para validação
+					 */
+					if($t.is('select')){ var tValido = validaSelect($t,o);}
+					else if($t.is(':checkbox')){var tValido = validaCheckboxRadio($t,o);}
+					else if($t.is(':radio')){var tValido = validaCheckboxRadio($t,o);}
+					else{var tValido = validaInput($t,o);}
+					
 					passa = passa && tValido;
 					
 					/**
@@ -171,21 +174,18 @@
 						}
 					}
 				}
-				
 			});
 		});
-		
 		return passa;
-		
 	};
 	
 	/**
-	 * Valida o input
+	 * Função para validar os campos Text, Password e Textarea
 	 * @param $t - $(this)
 	 * @param o - options
 	 */
 	function validaInput($t, o){
-			
+
 		var val = $t.val();
 		ret = true;
 		
@@ -194,37 +194,29 @@
 		 */
 		if($t.hasClass(o.classeEmail)){
 			var tmp = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/);
-			if(!tmp.test(val) || val==''){
-				ret = false;
-			}	
+			if(!tmp.test(val) || val==''){ret = false;}	
 		}
 		/**
 		 * URL
 		 */
-		else if(t.hasClass(o.classeUrl)){
+		else if($t.hasClass(o.classeUrl)){
 			var tmp = new RegExp(/^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/);
-			if(!tmp.test(val) || val==''){
-				ret = false;
-			}
+			if(!tmp.test(val) || val==''){ret = false;}
 		}
 		/**
 		 * Numeros
 		 */
-		if(t.hasClass(o.classeNumero)){	
+		if($t.hasClass(o.classeNumero)){	
 			var tmp = new RegExp(/(^-?\d\d*\.\d*$)|(^-?\d\d*$)|(^-?\.\d\d*$)|(^-?\d*$)/);
-			if(!tmp.test(val) || val==''){
-				ret = false;
-			}
+			if(!tmp.test(val) || val==''){ret = false;}
 		}
 		/**
 		 * REGEX Personalizado
 		 */
-		if(t.hasClass(o.classeRegex)){
-			var regex = t.attr(o.atributoRegex);	
+		if($t.hasClass(o.classeRegex)){
+			var regex = $t.attr(o.atributoRegex);	
 			var tmp = new RegExp(regex);
-			if(!tmp.test(val) || val==''){
-				ret = false;
-			}
+			if(!tmp.test(val) || val==''){ret = false;}
 		}
 		/**
 		 * Normal
@@ -234,6 +226,36 @@
 		}
 		
 		return ret;	
+	}
+	
+	/**
+	 * Função para validar os campos select
+	 * @param $t - $(this)
+	 * @param o - options
+	 */
+	function validaSelect($t, o){
+
+		if(!$t.val() || $t.val() == o.ValorSelectPadrao){
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Função para validar os campos do tipo Checkbox e Radio
+	 * Com suporte para multiplos checkbox/radios
+	 * @param $t - $(this)
+	 * @param o - options
+	 */
+	function validaCheckboxRadio($t, o){
+		
+		var inputName = $t.attr('name');
+		var checked = $("input[name="+inputName+"]:checked").length;
+
+        if(checked == 0){ 
+            return false; 
+        } 
+		return true;
 	}
 	
 })(jQuery);
