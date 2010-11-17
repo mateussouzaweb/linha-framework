99b27f80c5cef0049288bb30c6c44fd43b4c86eb
Mateus Souza <mateussouzaweb@gmail.com>
2010-11-17 17:17:42 -0200

    Nova versão, otimização de código anterior

--- desenvolvimento/js/src/acord.js
+++ desenvolvimento/js/src/acord.js
@@ -1,69 +1,66 @@
 /**
- * Acord 1.1
+ * Acord 1.2
  */
 (function($){
+
 	$.fn.acord = function(options){
-		return new $.acord(options, this);	
-	};
-	
-	$.acord = function(options, elem){
+		
 		var padrao = {
-				seletor: '.accordion',								//Seletor padrão, usado caso use o plugin sem o seletor $.plugin
-				pai: 'h2',											//Seletor pai, ou cabeçalho, header...
-				filho: 'div',										//Seletor filho, este é o que ficará escondido
+			pai: 'h2',											//Seletor pai, ou cabeçalho, header...
+			filho: 'div',										//Seletor filho, este é o que ficará escondido
 				
-				classePaiAtual: 'accordion-pai-atual', 				//Classe para pai que está em foque | Adicionado pelo plugin
-				classeFilhoAtual: 'accordion-filho-atual',			//Classe para o filho que esta visível | Adicionado pelo plugin
+			classePaiAtual: 'accordion-pai-atual', 				//Classe para pai que está em foque | Adicionado pelo plugin
+			classeFilhoAtual: 'accordion-filho-atual',			//Classe para o filho que esta visível | Adicionado pelo plugin
 				
-				classeAjax: 'ajax',									//Classe para accordions em ajax
-				atributoUrl: 'url',									//Atributo para url do accordion em ajax
+			classeAjax: 'ajax',									//Classe para accordions em ajax
+			atributoUrl: 'url',									//Atributo para url do accordion em ajax
 				
-				evento: 'click',									//Evento para disparar o efeito accordion
+			evento: 'click',									//Evento para disparar o efeito accordion
 				
-				inicial: 1, 										//Define o acordion que será exibido inicialmente ou default
-				sempreUm: true,										//Deixar sempre exibindo um seletor filho no accordion?
-				autoHeight: false,									//Ajustar automaticamente a altura dos elementos filho?
-				tempoIn: 'normal',									//Tempo para esconder o seletor filho (Entrada)
-				tempoOut: 'normal',									//Tempo para mostrar o seletor filho (Saída)
-				easingIn: 'swing',									//Animação com easing na entrada (IN)...
-				easingOut: 'swing',									//Animação com easing na saída (OUT)...
+			inicial: 1, 										//Define o acordion que será exibido inicialmente ou default
+			sempreUm: true,										//Deixar sempre exibindo um seletor filho no accordion?
+			autoHeight: false,									//Ajustar automaticamente a altura dos elementos filho?
+			tempoIn: 'normal',									//Tempo para esconder o seletor filho (Entrada)
+			tempoOut: 'normal',									//Tempo para mostrar o seletor filho (Saída)
+			easingIn: 'swing',									//Animação com easing na entrada (IN)...
+			easingOut: 'swing',									//Animação com easing na saída (OUT)...
 				
-				onAcord: null, 										//Callback
+			onAcord: null, 										//Callback
 				
-				live: false,										//Abilitar o monitoramento live
-				liveTempo: 100										//Tempo entra cada checagem, em milisegundos
+			live: false,										//Abilitar o monitoramento live
+			liveTempo: 150										//Tempo entra cada checagem, em milisegundos
 		};	
 		
 		var o = $.extend(padrao, options),
-			$d = $(document),
-			ap = o.classePaiAtual,
-			af = o.classeFilhoAtual;
-		
-		if(elem === undefined){elem = $(o.seletor);}
-		elem.elements = [];
-		
+			s = this.selector,
+			p = o.classePaiAtual,
+			f = o.classeFilhoAtual,
+			elems = [];
+
 		/**
 		 * Delegando eventos....o resultado seria o mesmo que each
 		 * Usado para registro em accordions futuros
 		 */
-		$d.delegate(elem.selector, 'iniciaAcord', function(){
+		$(document).delegate(s, 'iniciaAcord', function(){
 			
-			var elems = elem.elements,
-			els  =  $(elem.selector, elem.context),
-			nEls = els.not(elems);
-			elem.elements = els;
-
+			var	nEls = $(s).not(elems);
+				elems = $(s);
+				
 			/**
 			 * Retorna o processamento dos elementos q passaram
 			 */
 			nEls.each(function() {
 				var $t = $(this);
+				
 				/**
 				 * Fix para erros de animação
 				 */
-				$(o.filho, this).each(function(){$(this).css('height', $(this).height() + 'px').hide();});
-				if (o.sempreUm) {
-					$(o.filho, this).eq(o.inicial - 1).addClass(af).show().prev(o.pai).addClass(ap);
+				$(o.filho, this).each(function(){
+					$(this).css('height', $(this).height() + 'px').hide();
+				});
+				
+				if(o.sempreUm){
+					$(o.filho, this).eq(o.inicial - 1).addClass(f).show().prev(o.pai).addClass(p);
 				}
 				
 				/**
@@ -91,8 +88,8 @@
 				/**
 				 * Checa se o inicial é em ajax e já manda a requisição
 				 */
-				if(o.sempreUm && $('.'+ap, this).hasClass(o.classeAjax)){
-					$('.'+ap, this).trigger(o.evento);
+				if(o.sempreUm && $('.'+p, this).hasClass(o.classeAjax)){
+					$('.'+p, this).trigger(o.evento);
 				}
 			});
 			
@@ -103,10 +100,10 @@
 		 */
 		if(o.live){
 			setInterval(function(){
-				$(elem.selector).trigger('iniciaAcord');
+				$(s).trigger('iniciaAcord');
 			}, o.liveTempo);
 		}else{
-			if(elem.length){elem.trigger('iniciaAcord');}
+			if(this.length){this.trigger('iniciaAcord');}
 		}
 		
 		/**
@@ -144,10 +141,10 @@
 		 */
 		function animaAcord($acord, $pai, $filho){
 			
-			$(o.filho, $acord).removeClass(af);
-			$(o.pai, $acord).removeClass(ap);
-			$pai.addClass(ap);
-			$filho.addClass(af);
+			$('.'+f, $acord).removeClass(f);
+			$('.'+p, $acord).removeClass(p);
+			$pai.addClass(p);
+			$filho.addClass(f);
 			
 			/**
 			 * Callback
