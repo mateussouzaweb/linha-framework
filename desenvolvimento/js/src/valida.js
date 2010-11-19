81b8984183a6358a5b19edff4667013c35d121b8
Mateus Souza <mateussouzaweb@gmail.com>
2010-11-19 21:16:04 -0200

    Linha Valida 1.1 (Agora sim é 1.1)

--- desenvolvimento/js/src/valida.js
+++ desenvolvimento/js/src/valida.js
@@ -1,17 +1,11 @@
 /**
- * Valida 1.0
+ * Valida 1.1
  */
 (function($){
-	
+
 	$.fn.valida = function(options){
-		return new $.valida(options, this);	
-	};
-	
-	$.valida = function(options, elem){
 		
 		var padrao = {
-			seletor: '.formulario',					//Seletor padrão caso não haja nenhum, usado caso use o plugin sem o seletor $.plugin
-			
 			classeValidacao: 'valida', 				//Classe padrão para os inputs para validação
 			classeEmail: 'email',					//Classe para validar e-mails
 			classeUrl: 'url',						//Classe para validar urls
@@ -33,33 +27,29 @@
 			liveTempo: 100							//Tempo entra cada checagem, em milisegundos
 		};
 		var o = $.extend(padrao, options),
-			$d = $(document);
-		
-		if(elem === undefined){ elem = $(o.seletor);}
-		elem.elements = [];
+			s = this.selector,
+			elems = [];
 		
 		/**
 		 * Delega o Evento
 		 */
-		$d.delegate(elem.selector, 'iniciaValida', function(){
+		$(document).delegate(s, 'iniciaValida', function(){
 			
-			var elems = elem.elements,
-			els  =  $(elem.selector, elem.context),
-			nEls = els.not(elems);
-			elem.elements = els;
+			var	nEls = $(s).not(elems);
+			elems = $(s);
 			
 			/**
 			 * Processamento dos elementos q passaram
 			 */
 			nEls.each(function(){
-				
-				var $form = $(this);
-			
+							
 				/**
 				 * Checa se é um formulário
 				 */
-				if(!$(this).is('form')){ return false;}
+				if(!$(this).is('form')) return false;
 				
+				var $form = $(this);
+
 				/**
 				 * Inicia o processo de validação no click do submit
 				 */
@@ -68,7 +58,7 @@
 					/**
 					 * Começa a validação
 					 */
-					var resultado = $.valida.form(o, nEls);
+					var resultado = $.valida(o, $form);
 					
 					if(resultado){
 						if(!o.redireciona){
@@ -102,10 +92,10 @@
 		 */
 		if(o.live){
 			setInterval(function(){
-				$(elem.selector).trigger('iniciaValida');
+				$(s).trigger('iniciaValida');
 			}, o.liveTempo);
 		}else{
-			if(elem.length){elem.trigger('iniciaValida');}
+			if(this.length) this.trigger('iniciaValida');
 		}
 		
 	};
@@ -113,57 +103,55 @@
 	/**
 	 * Função para validar o formulário
 	 * @param o - options
-	 * @param elem - elemento
+	 * @param $form - elemento
 	 */
-	$.valida.form = function(o, elem){
+	$.valida = function(o, $form){
 	
 		var passa = true;
 		
 		/**
 		 * Faz o processo em cada entrada
 		 */
-		elem.each(function(){
-
-			$(this).find('input, textarea, select, checkbox, radio').not(':submit').each(function(){
+		$form.find('input, textarea, select, checkbox, radio').not(':submit').each(function(){
 				
-				var $t = $(this);
+			var $t = $(this);
 				
-				/**
-				 * Se tiver a classe necessária para validação
-				 */
-				if($t.hasClass(o.classeValidacao)){
+			/**
+			 * Se tiver a classe necessária para validação
+			 */
+			if($t.hasClass(o.classeValidacao)){
 					
+				/**
+				 * Callback
+				 */
+				if ($.isFunction(o.onValida)) {
+					o.onValida.apply(this, new Array($t, o));
+				}
+					
+				/**
+				 * Separa pelo tipo para validação
+				 */
+				if($t.is('select')) var tValido = validaSelect($t,o);
+				else if($t.is(':checkbox')) var tValido = validaCheckboxRadio($t,o);
+				else if($t.is(':radio')) var tValido = validaCheckboxRadio($t,o);
+				else var tValido = validaInput($t,o);
+					
+				passa = passa && tValido;
+					
+				/**
+				 * Se NÃO passar
+				 */
+				if(!tValido){
 					/**
 					 * Callback
 					 */
-					if ($.isFunction(o.onValida)) {
-						o.onValida.apply(this, new Array($t, o));
-					}
-					
-					/**
-					 * Separa pelo tipo para validação
-					 */
-					if($t.is('select')){ var tValido = validaSelect($t,o);}
-					else if($t.is(':checkbox')){var tValido = validaCheckboxRadio($t,o);}
-					else if($t.is(':radio')){var tValido = validaCheckboxRadio($t,o);}
-					else{var tValido = validaInput($t,o);}
-					
-					passa = passa && tValido;
-					
-					/**
-					 * Se NÃO passar
-					 */
-					if(!tValido){
-						/**
-						 * Callback
-						 */
-						if ($.isFunction(o.onErro)) {
-							o.onErro.apply(this, new Array($t, o));
-						}
+					if ($.isFunction(o.onErro)) {
+						o.onErro.apply(this, new Array($t, o));
 					}
 				}
-			});
+			}
 		});
+		
 		return passa;
 	};
 	
@@ -174,29 +162,28 @@
 	 */
 	function validaInput($t, o){
 
-		var val = $t.val();
-		ret = true;
-		
+		var val = $.trim($t.val());
+
 		/**
 		 * E-mail
 		 */
 		if($t.hasClass(o.classeEmail)){
 			var tmp = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/);
-			if(!tmp.test(val) || val==''){ret = false;}	
+			if(!tmp.test(val) || val=='') return false;	
 		}
 		/**
 		 * URL
 		 */
-		else if($t.hasClass(o.classeUrl)){
+		if($t.hasClass(o.classeUrl)){
 			var tmp = new RegExp(/^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/);
-			if(!tmp.test(val) || val==''){ret = false;}
+			if(!tmp.test(val) || val=='') return false;
 		}
 		/**
 		 * Numeros
 		 */
 		if($t.hasClass(o.classeNumero)){	
 			var tmp = new RegExp(/(^-?\d\d*\.\d*$)|(^-?\d\d*$)|(^-?\.\d\d*$)|(^-?\d*$)/);
-			if(!tmp.test(val) || val==''){ret = false;}
+			if(!tmp.test(val) || val=='') return false;
 		}
 		/**
 		 * REGEX Personalizado
@@ -204,16 +191,16 @@
 		if($t.hasClass(o.classeRegex)){
 			var regex = $t.attr(o.atributoRegex);	
 			var tmp = new RegExp(regex);
-			if(!tmp.test(val) || val==''){ret = false;}
+			if(!tmp.test(val) || val=='') return false;
 		}
 		/**
 		 * Normal
 		 */
 		else{
-			if(val==''){ret = false;};
+			if(val=='') return false;
 		}
 		
-		return ret;	
+		return true;
 	}
 	
 	/**
