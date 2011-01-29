317d1c8383fe659f541c36e0995a20cfef324a54
Mateus Souza <mateussouzaweb@gmail.com>
2011-01-29 19:29:15 -0200

    jQuery Linha Acord 1.3 Plugin totalmente remodelado, tornando-o mais estensível e mais parecido com os widgets da jQuery UI- extensão de exemplo incluído...

---
+++ desenvolvimento/js/src/jquery.acord.slide.js
@@ -0,0 +1,45 @@
+/**
+ * Extend para Acord 1.3+
+ * Simula um pouco o slide, aparecendo a descrição do item
+ */
+$.extend($.acord.padrao, {
+	descricao: '.descricao',	 		//Seletor para as descrições dos slides
+	tempoDescricaoIn: 'slow',			//Tempo para a entrada das descrições
+	tempoDescricaoOut: 'fast'			//Tempo para a saída das descrições
+}); 
+
+/**
+ * EXTEND slide init
+ */
+$.acord.padrao.extend['slide-init'] = function(){
+
+	var o = $(this).data('acord');
+	$(o.descricao, this).addClass('acord-descricao');
+
+};
+
+/**
+ * FX slide
+ */
+$.acord.fx['slide'] = function(p, f){
+	
+	var o = $(this).data('acord'),
+		self = this;
+				
+	/**
+	 * Esconde a descrição e o filho atual
+	 */
+	$('.acord-descricao', f).fadeOut(o.tempoDescricaoOut, function(){
+	
+		$(o.filho, self).not(f).slideUp(o.tempoIn, o.easingIn);
+		
+		/**
+		 * Exibe o filho e a descrição
+		 */	
+		f.slideDown(o.tempoOut, o.easingOut)
+		 .find('.acord-descricao')
+		 .hide()
+		 .fadeIn(o.tempoDescricaoIn);
+	});
+
+}
