fb5927694f7c36279a0485aa0455c39b8c06671b
Mateus Souza <mateussouzaweb@gmail.com>
2012-08-10 11:51:19 -0300

    Ajuste de código e comentários

--- html/js/plugins.js
+++ html/js/plugins.js
@@ -15,17 +15,13 @@
 
 	addClass(os[1] + ' ' + ua[1]);
 
-	/**
-	 * Fix Safari
-	 */
+	// Fix for Safari
 	if(ua[1] == 'safari')
 		addClass(ua[1] + '-' + ua[2].substring(0, 1));
 	else
 		addClass(ua[1] + '-' + parseInt(ua[2]));
 
-	/**
-	 * IE conditional
-	 */
+	// IE conditional
 	if(ua[1] == 'ie'){
 
 		for(var ver = 3; ver < 10; ver++) {
