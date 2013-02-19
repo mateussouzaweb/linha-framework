9544a0ce711590e394e3f9f6ea68997f65285856
Mateus Souza <mateussouzaweb@gmail.com>
2013-02-19 13:33:42 -0300

    Fix para seletores quando se está no modo App no iOS (ua não existe neste caso)

--- html/js/plugins.js
+++ html/js/plugins.js
@@ -5,28 +5,34 @@
 (function(){
 
 	var userAgent = navigator.userAgent.toLowerCase(),
-		html = document.getElementsByTagName('html')[0],
-		os = /(mac|win|linux|freebsd|mobile|iphone|ipod|ipad|android|blackberry|j2me|webtv)/.exec(userAgent),
-		ua = /(ie|firefox|chrome|safari|opera)(?:.*version)?(?:[ \/])?([\w.]+)/.exec(userAgent);
+		 html = document.getElementsByTagName('html')[0],
+		 os = /(mac|win|linux|freebsd|mobile|iphone|ipod|ipad|android|blackberry|j2me|webtv)/.exec(userAgent),
+		 ua = /(ie|firefox|chrome|safari|opera)(?:.*version)?(?:[ \/])?([\w.]+)/.exec(userAgent);
 
 	var addClass = function(name){
 		html.className += ' ' + name;
 	};
 
-	addClass(os[1] + ' ' + ua[1]);
+	addClass(os[1]);
 
-	// Fix for Safari
-	if(ua[1] == 'safari')
-		addClass(ua[1] + '-' + ua[2].substring(0, 1));
-	else
+	if( ua ){
+
+		addClass(ua[1]);
+
+		// Fix for Safari
+		if(ua[1] == 'safari')
+			addClass(ua[1] + '-' + ua[2].substring(0, 1));
+		else
 		addClass(ua[1] + '-' + parseInt(ua[2]));
 
-	// IE conditional
-	if(ua[1] == 'ie'){
+		// IE conditional
+		if(ua[1] == 'ie'){
 
-		for(var ver = 3; ver < 10; ver++) {
-			if(parseInt(ua[2]) < ver)
-				addClass('lt-ie-' + ver);
+			for(var ver = 3; ver < 10; ver++) {
+				if(parseInt(ua[2]) < ver)
+					addClass('lt-ie-' + ver);
+			}
+
 		}
 
 	}
