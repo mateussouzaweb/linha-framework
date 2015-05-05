e9d4890fbef3e6fe7dec3ae413d8ae25039527a6
Mateus Souza <mateussouzaweb@gmail.com>
2015-05-05 14:10:29 -0300

    Extra-selectors refatorado

--- js/framework.js
+++ js/framework.js
@@ -15,25 +15,27 @@
 
 	addClass(os[1]);
 
-	if( ua ){
+	if( !ua ){
+		return;
+	}
 
-		addClass(ua[1]);
+	addClass(ua[1]);
 
-		// Fix for Safari
-		if(ua[1] == 'safari')
-			addClass(ua[1] + '-' + ua[2].substring(0, 1));
-		else
-			addClass(ua[1] + '-' + parseInt(ua[2]));
+	// Fix for Safari
+	if( ua[1] == 'safari' ){
+		addClass(ua[1] + '-' + ua[2].substring(0, 1));
+	}else{
+		addClass(ua[1] + '-' + parseInt(ua[2]));
+	}
 
-		// IE conditional
-		if(ua[1] == 'ie'){
+	// IE conditional
+	if( ua[1] == 'ie' ){
 
-			for(var ver = 3; ver < 10; ver++) {
-				if(parseInt(ua[2]) < ver)
-					addClass('lt-ie-' + ver);
-			}
-
+		for(var ver = 3; ver < 10; ver++) {
+			if(parseInt(ua[2]) < ver)
+				addClass('lt-ie-' + ver);
 		}
+
 	}
 
 })();
